6daa19f63dacb51d8ceaca939d3ea0dac29b104e
ticlacalvin <ticlacalvin@yahoo.com>
2022-05-23 13:15:39 -0400

    created module s3

--- main.tf
+++ main.tf
@@ -1,23 +1,13 @@
-module "mymodule" {
-	source	 = "./modules/vpc_module"
-	vpc_cidr = "12.0.0.0/16"
-	private_subnet = "12.0.1.0/24"
-	public_subnet = "12.0.2.0/24"
+resource "aws_vpc" "myvpc" {
+	cidr_block = var.vpc_cidr
 }
 
-module "mymodule2" {
-        source   = "./modules/vpc_module"
-        vpc_cidr = "15.0.0.0/16"
-        private_subnet = "15.0.1.0/24"
-        public_subnet = "15.0.2.0/24"
+resource "aws_subnet" "private_subnet" {
+	vpc_id = aws_vpc.myvpc.id
+	cidr_block = var.private_subnet
 }
 
-module "mymodule3" {
-        source   = "./modules/vpc_module"
-        vpc_cidr = "19.0.0.0/16"
-        private_subnet = "19.0.1.0/24"
-        public_subnet = "19.0.2.0/24"
+resource "aws_subnet" "public_subnet" {
+        vpc_id = aws_vpc.myvpc.id
+        cidr_block = var.public_subnet
 }
-
-	
-	
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable vpc_cidr {
+	default = "10.0.0.0/16"
+}
+variable private_subnet {
+	default = "10.0.1.0/24"
+}
+variable public_subnet {
+	default = "10.0.2.0/24"
+}
