722c919a5f9e9a16846db9bcff04975a087c3d74
ticlacalvin <ticlacalvin@yahoo.com>
2022-05-23 13:30:02 -0400

    changed sources

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "mymodule" {
-	source	 = "./modules/vpc_module"
+	source	 = "git::https://github.com/ticlacalvin/forcing.git?ref=s3"
 	vpc_cidr = "12.0.0.0/16"
 	private_subnet = "12.0.1.0/24"
 	public_subnet = "12.0.2.0/24"
 }
 
 module "mymodule2" {
-        source   = "./modules/vpc_module"
+        source   = "git::https://github.com/ticlacalvin/forcing.git?ref=s3"
         vpc_cidr = "15.0.0.0/16"
         private_subnet = "15.0.1.0/24"
         public_subnet = "15.0.2.0/24"
 }
 
 module "mymodule3" {
-        source   = "./modules/vpc_module"
+        source   = "git::https://github.com/ticlacalvin/forcing.git?ref=s3"
         vpc_cidr = "19.0.0.0/16"
         private_subnet = "19.0.1.0/24"
         public_subnet = "19.0.2.0/24"
